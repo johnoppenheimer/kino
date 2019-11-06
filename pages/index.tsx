f376a8ea570b229f9aea6d97b2decfbe20c7f466
Maxime Cattet <maximecattet@gmx.com>
2019-11-06 18:33:19 +0100

    :construction: Do some UI on the home
    Using the `Layout`, the `Nav` and the `Container` previously created, I was able to pull together an alright looking really basic home page.
    
    The content of the home page only is so far a search field.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,11 @@
 import Head from 'next/head';
 import React from 'react';
-import { Container } from 'reactstrap';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'components/UI/Container';
+import Layout from 'components/UI/Layout';
+import Nav from 'components/UI/Nav';
+
+import 'styles/index.css';
 
 const Home = () => (
     <div>
@@ -10,9 +13,17 @@ const Home = () => (
             <title>Home</title>
             <link rel="icon" href="/favicon.ico" />
         </Head>
-
+        <Nav />
         <Container>
-            <h1>Hello world</h1>
+            <Layout>
+                <h1 className="text-white text-4xl text-bold">Search a movie or a show</h1>
+                <div className="block bg-gray-800 shadow-lg rounded p-6">
+                    <input
+                        className="bg-gray-700 appearance-none rounded w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline"
+                        placeholder="Citizen Kane"
+                    />
+                </div>
+            </Layout>
         </Container>
     </div>
 );
