805a5e252f361297300941827384ea062dcc8cac
Maxime Cattet <maximecattet@gmx.com>
2019-11-06 16:58:39 +0100

    :lipstick: Add colors to tailwind
    Add `plex` and `plex-dark` colors to the Tailwind config.
    
    Those two colors were taken from the plex website, the `dark` one being the hover mode.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
-  theme: {
-    extend: {}
-  },
-  variants: {},
-  plugins: []
-}
+    theme: {
+        extend: {
+            colors: {
+                plex: '#e19b0e',
+                'plex-dark': '#cd8e0f',
+            },
+        },
+    },
+    variants: {},
+    plugins: [],
+};
