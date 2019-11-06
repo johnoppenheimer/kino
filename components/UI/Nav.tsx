f9fad6f37b5c2463edcefe5fc91cc3ebba6be690
Maxime Cattet <maximecattet@gmx.com>
2019-11-06 18:15:55 +0100

    :sparkles: Create Nav component
    Delete the `nav` component that was on the nextjs boilerplate, and create a proper Tailwind one.
    
    The `Nav` is populated with some links (useless for now, but will be useful in the future once we add more pages).
    It use a state only on mobile to display or not the menu.

---
+++ components/UI/Nav.tsx
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+import classnames from 'classnames';
+
+const Nav = () => {
+    const [open, setOpen] = useState(false);
+
+    const toggle = () => {
+        setOpen(!open);
+    };
+
+    return (
+        <nav className="flex items-center justify-between flex-wrap bg-gray-800 p-6">
+            <div className="flex items-center flex-shrink-0 text-white mr-6">
+                <span className="font-semibold text-xl tracking-tight">Kino</span>
+            </div>
+            <div className="block lg:hidden">
+                <button
+                    className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white"
+                    onClick={toggle}
+                >
+                    <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                        <title>Menu</title>
+                        {open ? (
+                            <path d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z" />
+                        ) : (
+                            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+                        )}
+                    </svg>
+                </button>
+            </div>
+            <div
+                className={classnames('w-full flex-grow lg:flex lg:items-center lg:w-auto', {
+                    block: open,
+                    hidden: !open,
+                })}
+            >
+                <div className="text-sm lg:flex-grow">
+                    <a
+                        href="#responsive-header"
+                        className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
+                    >
+                        Search
+                    </a>
+                    <a
+                        href="#responsive-header"
+                        className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
+                    >
+                        Users
+                    </a>
+                    <a
+                        href="#responsive-header"
+                        className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white"
+                    >
+                        Issues
+                    </a>
+                </div>
+                <div>
+                    <a
+                        href="#responsive-header"
+                        className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white"
+                    >
+                        Me
+                    </a>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default Nav;
