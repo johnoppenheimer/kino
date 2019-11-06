fff101f3d1bcbffc322bfa895307740d1ec2e05e
Maxime Cattet <maximecattet@gmx.com>
2019-11-06 23:44:54 +0100

    :sparkes: Create a MovieCard
    The `MovieCard` component is a simple card to display a movie poster, its title and have a link to whatever we want (most likely to TMDb) when we click on the card.
    
    Use that card on the home page as a showcase

---
+++ components/UI/MovieCard.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+interface IMovieCardProps {
+    /** Title of the movie */
+    title: string;
+
+    /** Poster url */
+    imageUrl: string;
+
+    /** URL to redirect to */
+    url: string;
+}
+
+const MovieCard = ({ title, imageUrl, url }: IMovieCardProps) => (
+    <div className="lg:w-2/12 md:w-1/3 sm:w-1/2 p-2">
+        <a href={url}>
+            <div className="block bg-gray-800 shadow-lg rounded">
+                <img className="object-cover w-full" src={imageUrl} />
+            </div>
+            <p className="text-white py-2">{title}</p>
+        </a>
+    </div>
+);
+
+export default MovieCard;
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,9 +3,26 @@ import React from 'react';
 
 import Container from 'components/UI/Container';
 import Layout from 'components/UI/Layout';
+import MovieCard from 'components/UI/MovieCard';
 import Nav from 'components/UI/Nav';
 
 import 'styles/index.css';
+
+const FakeList = () => {
+    const list = [];
+
+    for (let i = 0; i < 12; i++) {
+        list.push(
+            <MovieCard
+                url="#"
+                title="Star Wars: The Rise of Skywalker"
+                imageUrl="https://image.tmdb.org/t/p/w500/db32LaOibwEliAmSL2jjDF6oDdj.jpg"
+            />,
+        );
+    }
+
+    return <>{list}</>;
+};
 
 const Home = () => (
     <div>
@@ -16,12 +33,20 @@ const Home = () => (
         <Container>
             <Nav />
             <Layout>
-                <h1 className="text-white text-4xl text-bold">Search a movie or a show</h1>
-                <div className="block bg-gray-800 shadow-lg rounded p-6">
-                    <input
-                        className="bg-gray-700 appearance-none rounded w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline"
-                        placeholder="Citizen Kane"
-                    />
+                <div className="mb-6">
+                    <h1 className="text-white text-4xl font-bold">Search a movie or a show</h1>
+                    <div className="block bg-gray-800 shadow-lg rounded p-6">
+                        <input
+                            className="bg-gray-700 appearance-none rounded w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline"
+                            placeholder="Citizen Kane"
+                        />
+                    </div>
+                </div>
+                <div className="mb-6">
+                    <h1 className="text-white text-4xl font-bold">Latest</h1>
+                    <div className="flex content-start flex-wrap">
+                        <FakeList />
+                    </div>
                 </div>
             </Layout>
         </Container>
