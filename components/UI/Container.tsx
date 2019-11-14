cdcf9244990b3b0f5fbfac3d727e5053ecac6e16
Maxime Cattet <maximecattet@gmx.com>
2019-11-14 20:04:06 +0100

    :recycle: Use KFC in Container
    As the container already implemented an optional `className` in its props, let’s use the newly created `KFC`.

--- components/UI/Container.tsx
+++ components/UI/Container.tsx
@@ -1,14 +1,11 @@
+import { KFC } from '@types';
 import React from 'react';
-
-interface IContainerProps {
-    className?: string;
-}
 
 /**
  * `Container` is the main background container that hold the background color
  */
-const Container: React.FunctionComponent<IContainerProps> = ({ children, className }) => (
-    <div className={`bg-gray-900 min-h-screen ${className}`}>{children}</div>
+const Container: KFC = ({ children, className }) => (
+    <div className={`text-white bg-gray-900 min-h-screen ${className}`}>{children}</div>
 );
 
 export default Container;
