7331381e552ed0aa9b644a7b78103f1ebf32efe1
Maxime Cattet <maximecattet@gmx.com>
2019-11-15 01:10:36 +0100

    :sparkles: Create a search api endpoint
    To avoid exposing the TMDb api and its key to the frontend, I created a search endpoint locally.

---
+++ pages/api/search.ts
@@ -0,0 +1,24 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+import tmdbClient from 'utils/tmdbClient';
+
+export default (req: NextApiRequest, res: NextApiResponse) => {
+    if (!req.query.search) {
+        return res.status(200).json([]);
+    }
+
+    tmdbClient('search/multi', {
+        params: {
+            api_key: process.env.TMDB_KEY,
+            languages: 'en-US',
+            query: req.query.search,
+            page: 1,
+        },
+    })
+        .then((response: any) => {
+            res.status(200).json(response.results);
+        })
+        .catch(error => {
+            res.status(error.response.status).json(error.response.data);
+        });
+};
