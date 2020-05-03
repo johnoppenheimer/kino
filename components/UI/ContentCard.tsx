17f5f388e835ebedd042f207e5ed2486803c8e73
Maxime Cattet <maximecattet@gmx.com>
2020-05-03 13:50:08 +0200

    :alien: Update ContentCard to use new Content model

--- components/UI/ContentCard.tsx
+++ components/UI/ContentCard.tsx
@@ -2,23 +2,15 @@ import classnames from 'classnames';
 import React from 'react';
 import { animated, config, useSpring } from 'react-spring';
 
-interface IContentCardProps {
-    /** Title of the content */
-    title: string;
+import Content from 'models/Content';
 
-    /** Poster url */
-    imageUrl: string;
-
-    /** URL to redirect to */
-    url: string;
-
-    /** Type of the content */
-    type: 'movie' | 'tv';
+export interface IContentCardProps {
+    content: Content;
 }
 
-const trans = s => `scale(${s})`;
+const trans = (s) => `scale(${s})`;
 
-const ContentCard = ({ title, imageUrl, url, type }: IContentCardProps) => {
+const ContentCard = ({ content }: IContentCardProps) => {
     const [props, set] = useSpring(() => ({ scale: 1, config: config.default }));
 
     const resetOnMouseLeave = () => {
@@ -29,9 +21,13 @@ const ContentCard = ({ title, imageUrl, url, type }: IContentCardProps) => {
         set({ scale: 1.05 });
     };
 
+    const imageURL = (() => {
+        return content.images[0]?.url ?? '';
+    })();
+
     return (
         <div className="lg:w-2/12 md:w-1/3 sm:w-1/2 p-2">
-            <a href={url}>
+            <a href="">
                 <animated.div
                     className="block bg-gray-800 w-full h-auto rounded overflow-hidden relative"
                     onMouseMove={onMouseMove}
@@ -42,15 +38,15 @@ const ContentCard = ({ title, imageUrl, url, type }: IContentCardProps) => {
                 >
                     <div
                         className={classnames('absolute text-xs py-1 px-2 rounded top-2 right-2', {
-                            'bg-red-700': type === 'movie',
-                            'bg-green-700': type === 'tv',
+                            'bg-red-700': content.type === 'movie',
+                            'bg-green-700': content.type === 'tv',
                         })}
                     >
-                        {type}
+                        {content.type}
                     </div>
-                    <img className="object-cover w-full shadow-lg" src={imageUrl} />
+                    <img className="object-cover w-full shadow-lg" src={imageURL} />
                 </animated.div>
-                <p className="text-white py-2">{title}</p>
+                <p className="text-white py-2">{content.title}</p>
             </a>
         </div>
     );
