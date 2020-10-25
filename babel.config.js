f913e2bd61b22c95ea32654dca98cc57bbe1d306
Maxime Cattet <maximecattet@gmx.com>
2020-10-25 18:02:12 +0100

    :wrench: Add store to babel aliases

--- babel.config.js
+++ babel.config.js
@@ -11,6 +11,7 @@ module.exports = (api) => {
                     utils: './utils',
                     hooks: './hooks',
                     models: './models',
+                    store: './store',
                 },
                 root: '.',
             },
