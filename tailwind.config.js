fb0b3e357b812aaa7415b8790721dae0db5ddb86
Maxime Cattet <maximecattet@gmx.com>
2020-10-25 19:14:30 +0100

    :rotating_light: Run linter to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,13 +8,13 @@ module.exports = {
         extend: {
             colors: {
                 plex: {
-                    '600': '#e19b0e',
-                    '700': '#cd8e0f',
+                    600: '#e19b0e',
+                    700: '#cd8e0f',
                 },
             },
         },
         inset: {
-            '2': '0.5rem',
+            2: '0.5rem',
         },
     },
     variants: {},
